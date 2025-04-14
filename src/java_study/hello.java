871ea41341ba0ad348925ab4c06e6b0b33659828
seokitt <jhslee4007@naver.com>
2025-04-14 16:45:00 +0900

    변수 개념, 변수 타입

---
+++ src/java_study/hello.java
@@ -0,0 +1,15 @@
+package java_study;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+		// 실행 (RUN) : Ctrl + 11F
+         // 주석  Ctrl + / 
+	 //  		CTRL + SIHFT + C
+	/*
+	 *  여러줄 주석
+	 */
+}
